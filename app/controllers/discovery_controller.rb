dc27d788ce2eb79e70ec4f105adb5190bb23be70
Tristan Burke <tristan_burke@berkeley.edu>
2017-10-03 08:49:54 -0700

    added redirect

---
+++ app/controllers/discovery_controller.rb
@@ -0,0 +1,26 @@
+class DiscoveryController < ApplicationController
+
+  def show
+  end
+
+  def index
+    redirect_to(discovery_path)
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :songs
+  resources :discovery
   # map '/' to be a redirect to '/discover'
-  root :to => redirect('/discover')
+
+  root 'discovery#index'
+
 end
