4de63f2b16974911ab8a2559bf031102296b01b9
Luke Strgar <lstrgar@berkeley.edu>
2017-10-03 14:30:37 -0700

    added routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,22 @@ Rails.application.routes.draw do
   resources :discovery
 
   # map '/' to be a redirect to '/discover#show'
-  root 'discovery#show'
+  root 'home#index'
+
+  namespace :app do
+    get '/browse', to: 'browse#show', as: 'browse'
+    get '/discovery', to: 'discovery#show', as: 'discovery'
+    post '/discovery/:id', to: 'discovery#favorite', as: 'discovery_favorite'
+    post '/discovery/toggle', 'discovery#toggle', as: 'discovery_toggle'
+    get '/discovery/next' => redirect('/discovery'), as: 'discovery_next'
+    get '/competition', to: 'competition#show', as: 'competition'
+    post '/competition/:id', to: 'competition#favorite', as: 'competition_favorite'
+    post '/competition/toggle/:id', to: 'competition#toggle'
+    get '/competition/next' => redirect('/competition'), as: 'competition_next'
+  end
+
+
+
+
 
 end
