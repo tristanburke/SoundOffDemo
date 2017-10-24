04161b6949b3c7bb457d290d582322c98e3d992f
Tristan Burke <tristan_burke@berkeley.edu>
2017-10-24 13:18:05 -0700

    added homepage, user Model

---
+++ app/controllers/homepage_controller.rb
@@ -0,0 +1,25 @@
+class HomepageController < ApplicationController
+
+  def show
+  end
+
+  def index
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # map '/' to be a redirect to '/discover#show'
-  root 'discovery#show'
+  root 'homepage#show'
 
+  get 'homepage/', to: 'homepage#show', as: 'homepage'
   get 'discovery/show', to: 'discovery#show', as: 'discovery_show'
   post 'discovery/next', to: 'discovery#next', as: 'discovery_next'
 end
